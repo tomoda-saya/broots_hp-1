ef0430271580566b96be9dcf29de88e5b2112afc
nexseed0729 <66339309+nexseed0729@users.noreply.github.com>
2021-09-20 12:44:59 +0900

    レスポンシブル化適用

--- app/resources/views/header.blade.php
+++ app/resources/views/header.blade.php
@@ -6,19 +6,26 @@
         <!-- <main_b id="container">
         <p> -->
     </div>
-    <span class="fa fa-bars menu-icon"></span>
-    <div class="header-right">
+    <!-- <span class="fa fa-bars menu-icon"></span> -->
+    <div class="header-center">
         <ul>
             <a href="#1">
                 <li class="li_a">NEWS</li>
+                <p class="header_line"></p>
             </a>
             <a href="#2">
                 <li class="li_b">SERVICE</li>
+                <p class="header_line"></p>
             </a>
             <a href="#3">
                 <li class="li_c">WORKS</li>
             </a>
-            <a href="home" class="recruit">
+        </ul>
+    </div>
+    {{-- <span class="fa fa-bars menu-icon"></span> --}}
+    <div class="header-right">
+        <ul>
+            <a href="recruit" class="recruit">
                 <li class="li_d">
                     <p>RECRUIT</p>
                 </li>
@@ -26,13 +33,13 @@
             <a href="contact" class="contact">
                 <li class="li_e">
                     <span class="mail_logo">
-                        <svg width="43" height="28" viewBox="0 0 43 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <svg width="3.3vw" height="1.9vw" viewBox="0 0 43 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <rect x="5.75039" y="6.30871" width="31.6219" height="20.8413" stroke="white" stroke-width="1.7"/>
                         <mask id="mask0" mask-type="alpha" maskUnits="userSpaceOnUse" x="2" y="4" width="39" height="15">
-                        <path d="M37.4559 5.81867L21.5 17.1567L5.5441 5.81866L37.4559 5.81867Z" stroke="white" stroke-width="1.7"/>
+                            <path d="M37.4559 5.81867L21.5 17.1567L5.5441 5.81866L37.4559 5.81867Z" stroke="white" stroke-width="1.7"/>
                         </mask>
                         <g mask="url(#mask0)">
-                        <rect x="2.75635" y="6.84302" width="37.4872" height="12.1282" fill="white"/>
+                            <rect x="2.75635" y="6.84302" width="37.4872" height="12.1282" fill="white"/>
                         </g>
                         </svg>
                     </span>
@@ -40,5 +47,31 @@
                 </li>
             </a>
         </ul>
+    </div>
+    <!--ハンバーガーメニュー-->
+    <div class="hamburger-menu">
+        <input type="checkbox" id="menu-btn-check">
+        <label for="menu-btn-check" class="menu-btn"><span></span></label>
+        <!--ここからメニュー-->
+        <div class="menu-content">
+            <ul>
+                <li>
+                    <a href="#1">NEWS</a>
+                </li>
+                <li>
+                    <a href="#2">SERVICE</a>
+                </li>
+                <li>
+                    <a href="#3">WORKS</a>
+                </li>
+                <li>
+                    <a href="recruit">RECRUIT</a>
+                </li>
+                <li>
+                    <a href="contact">CONTACT</a>
+                </li>
+            </ul>
+        </div>
+        <!--ここまでメニュー-->
     </div>
 </header>
